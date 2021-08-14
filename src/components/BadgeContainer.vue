a82c57d0332c9e5252c401a9d1d250968b692f0a
roy <roy@roygg.com>
2021-08-14 22:30:57 +0800

    ff

---
+++ src/components/BadgeContainer.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="container">
+    <ion-list>
+      <ion-list-header>Badges</ion-list-header>
+      <ion-item>
+        <ion-label>Followers</ion-label>
+        <ion-badge slot="end">22k</ion-badge>
+      </ion-item>
+      <ion-item>
+        <ion-label>Likes</ion-label>
+        <ion-badge color="secondary" slot="end">118k</ion-badge>
+      </ion-item>
+      <ion-item>
+        <ion-label>Stars</ion-label>
+        <ion-badge color="tertiary" slot="end">34k</ion-badge>
+      </ion-item>
+      <ion-item>
+        <ion-label>Completed</ion-label>
+        <ion-badge color="success" slot="end">80</ion-badge>
+      </ion-item>
+      <ion-item>
+        <ion-label>Warnings</ion-label>
+        <ion-badge>dfdfsdfsasdfsdfd</ion-badge>
+        <ion-badge>dfdfsdfsasdfsdfd</ion-badge>
+        
+        <ion-badge color="warning" slot="end">70</ion-badge>
+      </ion-item>
+      <ion-item>
+        <ion-label>Notifications</ion-label>
+        <ion-badge color="danger" slot="end">1000</ion-badge>
+      </ion-item>
+      <ion-item>
+        <ion-label>Unread</ion-label>
+        <ion-badge color="light" slot="end">24</ion-badge>
+      </ion-item>
+      <ion-item>
+        <ion-label>Drafts</ion-label>
+        <ion-badge color="medium" slot="end">14</ion-badge>
+      </ion-item>
+      <ion-item>
+        <ion-label>Deleted</ion-label>
+        <ion-badge color="dark" slot="end">4</ion-badge>
+      </ion-item>
+    </ion-list>
+  </div>
+</template>
+
+<script lang="ts">
+import { IonBadge, IonItem, IonLabel, IonList, IonListHeader } from '@ionic/vue';
+export default {
+  name: 'BadgeContainer',
+  components: {IonBadge, IonItem, IonLabel, IonList, IonListHeader },
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  color: #8c8c8c;
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+
+#container .sss {
+  background: none;
+  color: #000
+}
+</style>
--- src/views/Tab2.vue
+++ src/views/Tab2.vue
@@ -12,17 +12,17 @@
         </ion-toolbar>
       </ion-header>
       
-      <ExploreContainer name="Tab 2 page" />
+      <BadgeContainer name="Tab 2 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import BadgeContainer from '@/components/BadgeContainer.vue';
 
 export default  {
   name: 'Tab2',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { BadgeContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 }
 </script>
